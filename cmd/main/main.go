4c3810f51ba31562226a0d1098579bb369c6e629
NicoBarragan <nbarragan01@gmail.com>
2022-09-27 19:18:28 -0300

    feat(main.go): created an instance using the BussinessEmployee struct with its methods

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/NicoBarragan/go-intermediate-course/pkg/composition"
+)
+
+func main() {
+
+	nicoEmployee := composition.AddBussinessEmployee(20, "Nico", "Dev", 1, 2)
+	fmt.Println(nicoEmployee)
+
+	// bad
+	// nicoEmployee := BussinessEmployee{Person.Name: "Nico", Age: 20, Id: 0, YearsWorked: 2, Category: "Software Developer"}
+	// fmt.Println(nicoEmployee)
+
+	startWorking := nicoEmployee.StartWorking(true)
+	endWorking := nicoEmployee.EndsWorking(true)
+
+	fmt.Println("startWorking: ", startWorking)
+	fmt.Println("endWorking: ", endWorking)
+}
