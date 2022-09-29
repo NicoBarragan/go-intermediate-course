a3f35cdbd3615029b7071f38dd5f6c8846f2b8bb
NicoBarragan <nbarragan01@gmail.com>
2022-09-29 18:02:46 -0300

    feat(functions.go): added variadic and named returns func's with logs

--- pkg/functions/functions.go
+++ pkg/functions/functions.go
@@ -5,20 +5,21 @@ import (
 	"time"
 )
 
-func lambda() {
+// anon func's are not very recommended for DRY purposes. Only use it when it is necessary.
+func anon() {
 	x := 5
 
 	// Anonymous func 1
 	y := func(x int) int {
 		return x * 2
 	}
-	fmt.Println("y lambda func: ", y(x))
+	fmt.Println("y anon func: ", y(x))
 
 	// Anonymous func 2 (it calls directly the param and calls itself)
 	z := func() int {
 		return x * 2
 	}()
-	fmt.Println("y lambda func: ", z)
+	fmt.Println("y anon func: ", z)
 
 	// 3rd Anonymous way: lambda Goroutine in a channel
 	// Create channel for blocking Gorouting until msg arrives
@@ -38,6 +39,49 @@ func lambda() {
 
 }
 
+///////////////////////
+
+/* Variadic funcs */
+// Variadic func able us to insert a dinamic amount of params
+// The param is a slice of the defined type
+func sum(numbers ...int) int {
+	totalSum := 0
+	for _, num := range numbers {
+		totalSum += num
+	}
+
+	return totalSum
+}
+
+func getNames(names ...string) {
+	for _, name := range names {
+		fmt.Println(name)
+	}
+}
+
+///////////////////////
+/* Named returns func's */
+func multiples(a int8) (double, triple, quad int8) {
+	double = a * 2
+	triple = a * 3
+	quad = a * 4
+	return
+}
+
 func Functions() {
-	lambda()
+	anon()
+
+	fmt.Println("----------------")
+	fmt.Println(sum(1, 2, 3))
+	fmt.Println(sum(1, 2, 3, 4, 5, 6))
+
+	fmt.Println("----------------")
+	getNames("Nico", "Pedro")
+	getNames("Nico", "Pedro", "Facu")
+
+	fmt.Println("----------------")
+	double, triple, quad := multiples(2)
+	// %o is for base 8 int
+	fmt.Printf("Double: %o, triple: %o, quad: %o ", double, triple, quad)
+
 }
